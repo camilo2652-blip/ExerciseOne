10ea16f5ba8e0f267f3ea9f57ceb8231b6b5270f
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-07 21:54:02 -0500

    Exercise 01 finesh

---
+++ EjerciciosC#/numberIsEvenOrNot/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+do
+{
+    Console.Write("Ingrese número o Ctrl + c para salir: ");
+    var numberString = Console.ReadLine();
+    try
+    {
+        var numberInt = int.Parse(numberString!);
+        if (numberInt % 2 == 0) 
+        {
+            Console.WriteLine($"El número: {numberInt} es par.");
+        }
+        else
+        {
+            Console.WriteLine($"El número: {numberInt} es impar.");
+        }
+    }
+    catch 
+    {
+        Console.WriteLine($"El número: {numberString} no es válido. Solo utilice carácteres numéricos");
+    }
+} while (true); 
