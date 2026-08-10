85fcdaf244f4f59f8226543389f9fe9f94ae2a52
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-09 21:51:17 -0500

    IsMultipleOfAnother finish

---
+++ EjerciciosC#/IsMultipleOfAnother/Program.cs
@@ -0,0 +1,19 @@
+﻿using Shared;
+
+
+do
+{
+    Console.WriteLine("Ingrese 2 números");
+    var a = ConsoleExtension.GetInt("Ingresa primer número: ");
+    var b = ConsoleExtension.GetInt("Ingresa segundo número: ");
+ 
+    if (b % a == 0) 
+    {
+        Console.WriteLine($"{a} es multiplo de {b}");
+    }
+    else
+    {
+        Console.WriteLine($"{a} no es multiplo de {b}");
+    }
+
+}while (true);
