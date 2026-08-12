94252e5dfe5e2cf04aca49906db8a61bac3cc453
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-11 23:50:47 -0500

    calculations

--- EjerciciosC#/simplePayroll/Program.cs
+++ EjerciciosC#/simplePayroll/Program.cs
@@ -5,11 +5,22 @@ var options = new List<string> { "s", "n" };
 
 do
 {
-    var name = ConsoleExtension.GetString("Ingrese nombre: ");
-    var worHours = ConsoleExtension.GetFloat("Ingrese número horas trabajadas: ");
-    var hourValue = ConsoleExtension.GetDecimal("Ingrese valor hora: ");
+    var name = ConsoleExtension.GetString("Ingrese nombre......................: ");
+    var workHours = ConsoleExtension.GetFloat("Ingrese número horas trabajadas.....: ");
+    var hourValue = ConsoleExtension.GetDecimal("Ingrese valor hora..................: ");
     var salaryMinimun = ConsoleExtension.GetDecimal("Ingrese valor salario mínimo mensual: ");
 
+    var Salary = (decimal)workHours * hourValue;
+    if ((Salary < salaryMinimun))
+    {
+        Console.WriteLine($"Nombre..............................: {name}");
+        Console.WriteLine($"Salario.............................: {salaryMinimun:C2}");
+    }
+    else
+    {
+        Console.WriteLine($"Nombre..............................: {name}");
+        Console.WriteLine($"Salario.............................: {Salary:C2}");
+    }
     do
     {
         answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
