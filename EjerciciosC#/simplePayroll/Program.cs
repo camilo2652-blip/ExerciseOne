10392c9e4cf924aa978693bb2a4138f2e4c3cea1
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-11 23:31:14 -0500

    InitialConfiguration

---
+++ EjerciciosC#/simplePayroll/Program.cs
@@ -0,0 +1,19 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var name = ConsoleExtension.GetString("Ingrese nombre: ");
+    var worHours = ConsoleExtension.GetFloat("Ingrese número horas trabajadas: ");
+    var hourValue = ConsoleExtension.GetDecimal("Ingrese valor hora: ");
+    var salaryMinimun = ConsoleExtension.GetDecimal("Ingrese valor salario mínimo mensual: ");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game over");
