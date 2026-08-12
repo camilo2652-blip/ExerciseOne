de50ea631da83115cd9d1d138da8f27de20be5dc
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-11 23:08:07 -0500

    goOut

--- EjerciciosC#/OrderingOfThreeNumbers/Program.cs
+++ EjerciciosC#/OrderingOfThreeNumbers/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Shared;
 
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
 do
 {
     Console.WriteLine("Ingrese 3 números diferentes");
@@ -50,4 +53,10 @@ do
             Console.WriteLine($"El mayor es {c}, el medio es {b}, el menor es {a}");
         }
     }
-}while (true);
+do
+{
+    answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase)) ;
+Console.WriteLine("Game over");
