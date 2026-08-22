37898b4967a32275ab313fb27c387ba6dd5d97db
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-22 17:45:07 -0500

    Taylor finish

---
+++ EjerciciosC#/Taylor/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using Shared;
+using System.Diagnostics.CodeAnalysis;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+
+    var n = ConsoleExtension.GetInt("Cuántos términso desea: ");
+    var x = ConsoleExtension.GetDouble("Digita el valor de x..: ");
+    var taylor = Taylor(x, n);
+
+    Console.WriteLine($"f({x}) = {taylor:N5}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game over");
+
+double Taylor(double x, int n)
+{
+    double sum = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        sum += Math.Pow(x, i) / MyMath.Factorial(i);
+    }
+    return sum;
+
+}
+
+
