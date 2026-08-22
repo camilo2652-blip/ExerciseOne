11536669154ee6ef90a9e3494de58c5959e20a91
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-22 17:32:03 -0500

    Factorial Finish

---
+++ EjerciciosC#/Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+
+    var n = ConsoleExtension.GetInt("Ingrese número: ");
+    var factorial = MyMath.Factorial(n);
+    Console.WriteLine($"{n}! = {factorial:N0}");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game over");
