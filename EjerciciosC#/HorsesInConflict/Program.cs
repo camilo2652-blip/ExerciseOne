f7dca47e5a67d15b4e509d31f708cff417cfd9a1
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-21 23:57:04 -0500

    sevaral

---
+++ EjerciciosC#/HorsesInConflict/Program.cs
@@ -0,0 +1,51 @@
+﻿using Shared;
+
+var ubicaciones = ConsoleExtension.GetString(
+    "Ingrese ubicación de los caballos: ")!;
+
+var caballos = ubicaciones.Split(',');
+
+foreach (var caballo in caballos)
+{
+    var posicion = caballo.Trim().ToUpper();
+
+    char columna = posicion[0];
+    int fila = int.Parse(posicion.Substring(1));
+
+    Console.Write($"Analizando Caballo en {fila}{columna} =>");
+
+    var conflictos = new List<string>();
+
+    foreach (var otroCaballo in caballos)
+    {
+        var otraPosicion = otroCaballo.Trim().ToUpper();
+
+        if (posicion == otraPosicion)
+        {
+            continue;
+        }
+
+        char otraColumna = otraPosicion[0];
+        int otraFila = int.Parse(otraPosicion.Substring(1));
+
+        int diferenciaColumnas = Math.Abs(columna - otraColumna);
+        int diferenciaFilas = Math.Abs(fila - otraFila);
+
+        if ((diferenciaColumnas == 1 && diferenciaFilas == 2) ||
+            (diferenciaColumnas == 2 && diferenciaFilas == 1))
+        {
+            conflictos.Add($"{otraFila}{otraColumna}");
+        }
+    }
+
+    conflictos = conflictos
+        .OrderByDescending(x => int.Parse(x.Substring(0, 1)))
+        .ToList();
+
+    foreach (var conflicto in conflictos)
+    {
+        Console.Write($" Conflicto con {conflicto}");
+    }
+
+    Console.WriteLine();
+}
---
+++ EjerciciosC#/TheStrongestBeam/Program.cs
@@ -0,0 +1,110 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var beam = ConsoleExtension.GetString("Ingrese la viga: ");
+
+    if (!IsValidBeam(beam))
+    {
+        Console.WriteLine("La viga está mal construida!");
+    }
+    else
+    {
+        var resistance = GetResistance(beam);
+        var weight = CalculateWeight(beam);
+
+        if (weight <= resistance)
+        {
+            Console.WriteLine("La viga soporta el peso!");
+        }
+        else
+        {
+            Console.WriteLine("La viga NO soporta el peso!");
+        }
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions(
+            "Deseas continuar [S]i, [N]o?.......: ",
+            options);
+
+    } while (!options.Any(x =>
+        x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+
+Console.WriteLine("Game over");
+
+
+bool IsValidBeam(string beam)
+{
+    if (beam.Length == 0)
+    {
+        return false;
+    }
+
+    if (beam[0] != '%' && beam[0] != '&' && beam[0] != '#')
+    {
+        return false;
+    }
+
+    for (int i = 1; i < beam.Length; i++)
+    {
+        if (beam[i] != '=' && beam[i] != '*')
+        {
+            return false;
+        }
+
+        if (beam[i] == '*' && i + 1 < beam.Length && beam[i + 1] == '*')
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+int GetResistance(string beam)
+{
+    if (beam[0] == '%')
+    {
+        return 10;
+    }
+
+    if (beam[0] == '&')
+    {
+        return 30;
+    }
+
+    return 90;
+}
+
+
+int CalculateWeight(string beam)
+{
+    var weight = 0;
+    var sequence = 0;
+
+    for (int i = 1; i < beam.Length; i++)
+    {
+        if (beam[i] == '=')
+        {
+            sequence++;
+        }
+        else
+        {
+            weight += sequence;
+            weight += sequence * 2;
+            sequence = 0;
+        }
+    }
+
+    weight += sequence;
+
+    return weight;
+}
