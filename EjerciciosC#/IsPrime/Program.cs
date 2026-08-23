eac7a1f2d02234b2c1d3ab583b597ea3eaf86685
Juan Camilo Mora <camilo2652@gmaill.com>
2026-08-22 23:22:22 -0500

    IsPrime finish

---
+++ EjerciciosC#/IsPrime/Program.cs
@@ -0,0 +1,24 @@
+﻿using Shared;
+
+var answer = string.Empty;
+var options = new List<string> { "s", "n" };
+
+do
+{
+    var n = ConsoleExtension.GetInt("Ingresa número: ");
+    var isPrime = MyMath.IsPrime(n);
+
+    Console.WriteLine($"El número: {n} {(isPrime ? "SI" : "NO")} es primo.");
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Deseas continuar [S]i [n]o?: ", options);
+        Console.WriteLine();
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("s", StringComparison.CurrentCultureIgnoreCase));
+Console.WriteLine("Game over");
+
+
+
+
